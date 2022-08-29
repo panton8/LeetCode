cf8aad4fa967a6097bcd5e7223185c1d282cd695
Anton Padvalnikau <podvalnikov0@gmail.com>
2022-08-30 01:49:38 +0300

    improve execution time

--- task_1046_lastStoneWeight.py
+++ task_1046_lastStoneWeight.py
@@ -1,23 +1,23 @@
 def lastStoneWeight(stones: list[int]) -> int:
-    stones.sort()
-
     while len(stones) > 1:
-        st1, st2 = stones[-1], stones[-2]
-
-        stones.remove(stones[-1])
-        stones.remove(stones[-1])
+        st1 = max(stones)
+        stones.remove(max(stones))
+        st2 = max(stones)
+        stones.remove(max(stones))
 
         if st1 - st2 > 0:
             stones.append(st1 - st2)
-
-        stones.sort()
 
     if stones:
         return stones[0]
     return 0
 
+
 def main():
     print(lastStoneWeight([2, 7, 4, 1, 8, 1]))
+
+    assert lastStoneWeight([1]) == 1
+
 
 if __name__ == "__main__":
     main()
